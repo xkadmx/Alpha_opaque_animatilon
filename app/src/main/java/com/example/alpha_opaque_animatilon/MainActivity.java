b8a1643ff83ab5254893845ac1ffabd4438156e1
Zuzanna <zuzanna.rosinska@yahoo.com>
2019-04-14 10:52:53 +0200

    Initial commit

---
+++ app/src/main/java/com/example/alpha_opaque_animatilon/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.alpha_opaque_animatilon;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
